74a4f8a282257305f1e457fec6b67c5fc624a0dd
ElGuerreroDragon <lucas25monzon@gmail.com>
2023-04-14 20:36:33 -0300

    Cambios varios
    Agregue js para un alert al descargar mi CV.
    
    Agregue mi proyecto uno y los bloques que faltaban para Lucas Ponce y mios

---
+++ main.js
@@ -0,0 +1,8 @@
+function notification(){
+    swal("Oh no 😢", "...No tenemos disponible ningun C.V para descargar");
+}
+setTimeout(() => {
+    var fotos = document.getElementById("imgprofile")
+
+
+}, 2000);
